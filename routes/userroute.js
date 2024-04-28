4716fc937d1f761e53f625ae5028508e6760a7f5
laxmanballa <laxman@wielabs.com>
2024-04-28 14:56:08 +0530

    added delete user api

--- routes/userroute.js
+++ routes/userroute.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { updateUser } from "../controllers/usercontroller.js";
+import { DeleteUser, updateUser } from "../controllers/usercontroller.js";
 import VerifyToken from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.put("/update/:userId", VerifyToken, updateUser);
-router.delete("/delete/:userId", VerifyToken);
+router.delete("/delete/:userId", VerifyToken, DeleteUser);
 
 export default router;
