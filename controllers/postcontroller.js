701e4191ca3d691d7f8488e36a4d37b2ec38231d
laxmanballa <laxman@wielabs.com>
2024-05-05 15:11:39 +0530

    created post api

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -2,13 +2,13 @@ import Post from "../models/postmodel.js";
 import { ErrorHandler } from "../utils/error.js";
 
 export const CreatePost = async (req, res, next) => {
-  if (!req.body.isAdmin) {
+  if (!req.user.isAdmin) {
     return next(ErrorHandler(403, "You do not have permission to Create Post"));
   }
   if (!req.body.title || !req.body.content) {
     return next(ErrorHandler(400, "All Fields are Required"));
   }
-  const slug = req.body
+  const slug = req.body.title
     .split(" ")
     .join("-")
     .toLowerCase()
@@ -17,7 +17,7 @@ export const CreatePost = async (req, res, next) => {
   const newPost = new Post({
     ...req.body,
     slug,
-    userId: req.body.userId
+    userId: req.user.id
   });
   try {
     const savedPost = await newPost.save();
