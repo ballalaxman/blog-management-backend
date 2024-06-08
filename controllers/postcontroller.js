8b8e32c7e1dbf6ee27d6322ff7d7c8d10abb4407
laxmanballa <laxman@wielabs.com>
2024-06-08 18:21:52 +0530

    created apis for delete and update post

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -83,3 +83,40 @@ export const GetPosts = async (req, res, next) => {
     next(error);
   }
 };
+
+export const DeletePost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(ErrorHandler(403, "You are not allowed to delete the post"));
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json("The Post has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const UpdatePost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(ErrorHandler(403, "You are not allowed to delete the post"));
+  }
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image
+        }
+      },
+      { new: true },
+      { upsert: true }
+    );
+
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/postroute.js
+++ routes/postroute.js
@@ -1,10 +1,17 @@
 import express from "express";
 import VerifyToken from "../utils/verifyUser.js";
-import { CreatePost, GetPosts } from "../controllers/postcontroller.js";
+import {
+  CreatePost,
+  DeletePost,
+  GetPosts,
+  UpdatePost
+} from "../controllers/postcontroller.js";
 
 const postRouter = express.Router();
 
 postRouter.post("/create", VerifyToken, CreatePost);
 postRouter.get("/getposts", GetPosts);
+postRouter.delete("/deletepost/:postId/:userId", VerifyToken, DeletePost);
+postRouter.put("/updatepost/:postId/:userId", VerifyToken, UpdatePost);
 
 export default postRouter;
